cbad0bcae64cd05062fc35f9b6f57c247d478de8
Teguhriyadi <ilham.teguh55@gmail.com>
2023-06-27 19:21:39 +0700

    Update Logo di title

--- resources/views/page/autentikasi/login.blade.php
+++ resources/views/page/autentikasi/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="author" content="Kodinger">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Login Aplikasi</title>
+    <link rel="icon" type="image/png" href="{{ url('/image/logo-polindra.png') }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
